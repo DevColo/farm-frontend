ba388548027b50d31721f8195316a971940cc357
Thomas Gayflor Jr <58783397+DevColo@users.noreply.github.com>
2025-07-02 00:36:26 +0200

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://api.jakaja.net/api',
+  baseURL: 'https://api.jakaja.net',
   //baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
